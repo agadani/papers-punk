3ecea639ab2837986775ec36c4eaa3f6aff416f1
avani <avani@salk.edu>
2015-02-16 15:16:36 -0800

    Clean up formatting, skip infinite loop of new tabs, avoid int.toString.

--- Directorylister.js
+++ Directorylister.js
@@ -9,22 +9,20 @@ DirectoryList.prototype.getFileList = function( fileListener ) {
   var $this = this;
   chrome.extension.onMessage.addListener( function(request, sender, sendResponse) {
     chrome.extension.onMessage.removeListener(arguments.callee);
-    chrome.tabs.remove($this.tabid);
+    chrome.tabs.remove($this.directory_list_tab);
     $this.fileList = request;
     fileListener(request);
   });
 
 
   chrome.tabs.onUpdated.addListener(function onTabUpdated(tabId, changeInfo, _tab) {
-    if ( _tab.id.toString() === $this.tabid.toString() ) {
+    if ( _tab.id == $this.directory_list_tab ) {
       chrome.tabs.executeScript(_tab.id, {file: "directorylister-tab.js"});
       chrome.tabs.onUpdated.removeListener(arguments.callee);
     } 
   });
 
-  chrome.tabs.create({}, function(_tab) {
-    $this.tabid = _tab.id;
-    console.log("Creating listing tab (" + $this.tabid + ") for: " + $this.path);
-    chrome.tabs.update(_tab.id, {url:"file://"+$this.path, selected:false});
+  chrome.tabs.create({url:"file://"+$this.path, selected:false}, function(_tab) {
+    $this.directory_list_tab = _tab.id;
   });
 };
